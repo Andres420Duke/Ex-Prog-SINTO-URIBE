c6113f66428ed953ffc64e69b0c197bac08bc957
Andres Sinto Duke <82553967+Andres420Duke@users.noreply.github.com>
2021-05-12 21:44:46 -0500

    Update Ex1.c

--- Ex1.c
+++ Ex1.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main
+int main ()
 {
-    /*Sinto uribe omar andres Ex1*/
-   
-   int primer, segundo, multiplica, division;
-   printf ("\ n\ n Programa 3");
-   printf ("\ n\ n Saca el promedio de valores reales");
-   printf ("\ n\ n Dame el primer valor real");
-   scanf ("%f", &primer);
-   printf ("\ n\ n Dame el segundo valor real");
-   scanf ("%f", &segundo);
-   multiplica = primer * segundo;
-   division = primer / segundo;
-   printf ("\ n El producto es %f", multiplica);
-   printf ("\ n El cociente es %f", division);
-   return 0;
+    /* Martínez Lira Irak Emmanuel 4IV17
+       */
+
+    float primer, segundo, multiplica, division;
+    printf("\n\n MARTÍNEZ LIRA IRAK EMMANUEL 4IV17");
+    printf("\n\n Programa 3");
+    printf("\n\n Saca el promedio de valores reales");
+    printf("\n\n Dame el primer valor real");
+    scanf("%f", &primer);
+    printf("\n\n Dame el segundo valor real");
+    scanf("%f", &segundo);
+    multiplica = primer * segundo;
+    division = primer / segundo;
+    printf("\n El producto es %f", multiplica);
+    printf("\n El cociente es %f", division);
+    return 0;
 }
 
