5f008b27af115007fd3dba6e9e4dfc0e965b0f35
Andres Sinto Duke <82553967+Andres420Duke@users.noreply.github.com>
2021-05-12 21:46:05 -0500

    Update Ex4.c

--- Ex4.c
+++ Ex4.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int main  ()
+int main ()
 {
-    /*Sinto uribe omar andres Ex4*/
-
-   float peso, altura, IMC;
-   Printf ("\n\n Programa 7");
-   printf ("\n\n Calcular el indice de masa corporal");
-   printf ("\n\n Dame el peso");
-   scanf ("%f", &peso);
-   printf ("\n\n Dame la altura");
-   scanf ("%f", &altura);
-   IMC = peso / (altura*altura);
-   printf ("\n La masa muscular es %f", IMC);
-   return 0;
+   
+    float peso, altura, IMC;
+    printf("\n SINTO URIBE OMAR ANDRES 4IV17");
+    printf("\n\n Programa 4");
+    printf("\n\n Calcular el indice de masa corporal");
+    printf("\n\n Dame el peso");
+    scanf("%f", &peso);
+    printf("\n\n Dame la altura");
+    scanf("%f", &altura);
+    IMC= peso /(altura*altura);
+    printf("\n lA MASA MUSCULAR ES %f", IMC);
+    
+    return 0;
 }
